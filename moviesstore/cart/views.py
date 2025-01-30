a5f218f489d9149c7d0a342e738cb8f8b3178981
Mahika-raj <mraj39@gatech.edu>
2025-01-29 22:26:17 -0500

    add template code

---
+++ moviesstore/cart/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from movies.models import Movie
+from .utils import calculate_cart_total
+
+
+def add(request, id):
+    get_object_or_404(Movie, id=id)
+    cart = request.session.get('cart', {})
+    cart[id] = request.POST['quantity']
+    request.session['cart'] = cart
+    return redirect('home.index')
+
+def add_to_cart(request, id):
+    get_object_or_404(Movie, id=id)
+    cart = request.session.get('cart', {})
+    cart[id] = request.POST['quantity']
+    request.session['cart'] = cart
+    return redirect('cart.index')
