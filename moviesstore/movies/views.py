6d9a88656d4088d8e94d91bfd1e15e0aa28dff44
KseniiaSuleimanova <kseniiasuleimanova@gmail.com>
2025-02-06 13:20:28 -0500

    movies page fixed

--- moviesstore/movies/views.py
+++ moviesstore/movies/views.py
@@ -4,18 +4,18 @@ from .models import Movie
 def index(request):
     search_term = request.GET.get('search')
     if search_term:
-        movies =
-        Movie.objects.filter(name__icontains=search_term)
+        movies = Movie.objects.filter(name__icontains=search_term)
     else:
         movies = Movie.objects.all()
     template_data = {}
     template_data['title'] = 'Movies'
-    template_data['movies'] = 'movies'
+    template_data['movies'] = Movie.objects.all()
+    print(template_data)
     return render(request, 'movies/index.html',
                   {'template_data': template_data})
 
 def show(request, id):
-    movie =  Movie.objects.get(id=id)
+    movie = Movie.objects.get(id=id)
     template_data = {}
     template_data['title'] = movie.name
     template_data['movie'] = movie
