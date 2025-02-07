9520d62078b166dde669b0ed2d39bb811c26e3c3
hannahrajan <66148049+hannahrajan@users.noreply.github.com>
2025-02-07 16:34:51 -0500

    Link on Home Page + Graphics
    - Added Cat Graphics
    - Added Movies Tab to base template
    - Added link to movies on home page

--- moviesstore/movies/urls.py
+++ moviesstore/movies/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
+app_name = 'movies'
+
 urlpatterns = [
     path('', views.index, name='movies.index'),
     path('<int:id>/', views.show, name='movies.show'),
