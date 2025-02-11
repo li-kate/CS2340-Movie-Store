a8ff55f0bb1828dcdea7253fa4f5cc1b12cbf47d
KseniiaSuleimanova <kseniiasuleimanova@gmail.com>
2025-02-10 23:29:25 -0500

    deleting reviews works

--- moviesstore/movies/urls.py
+++ moviesstore/movies/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:id>/', views.show, name='show'),
     path('<int:id>/review/create/', views.create_review, name='create_review'),
     path('<int:id>/review/<int:review_id>/edit/', views.edit_review, name='edit_review'),
+    path('<int:id>/review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
 ]
--- moviesstore/movies/views.py
+++ moviesstore/movies/views.py
@@ -56,3 +56,10 @@ def edit_review(request, id, review_id):
         return redirect('movies:show', id=id)
     else:
         return redirect('movies:show', id=id)
+
+@login_required
+def delete_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id,
+        user=request.user)
+    review.delete()
+    return redirect('movies:show', id=id)
